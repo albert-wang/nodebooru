ba48adbbb8a63e253c7e9235f9a6e06d87cde9c5
Albert Wang <albertwang@ironclad.mobi>
2011-12-12 15:41:34 -0600

    index

---
+++ index.js
@@ -0,0 +1,23 @@
+var booru     = require("./obooru")
+var http      = require("http")
+
+http.requireServer(function(request, result) {
+
+
+}).listen(3000)
+console.log("Server is now listening on port 3000")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
